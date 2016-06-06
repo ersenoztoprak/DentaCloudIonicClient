4bd179be43a6e3ee5c30065a111cc21e42604f45
Ersen Öztoprak <hersenoztoprak@yahoo.com>
2016-06-06 23:01:44 +0300

    implemented form validations

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('DentaCloud', ['ionic', 'DentaCloud.controllers', 'DentaCloud.services'])
+angular.module('DentaCloud', ['ionic', 'ngMessages', 'DentaCloud.controllers', 'DentaCloud.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
